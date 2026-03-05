996229d697dcf225d049078d5dd4b4003967d54c
snickodonnell <snickodonnell@gmail.com>
2026-03-05 15:49:32 -0600

    Added item structure and initial weapons data.

--- data/items/ItemResource.gd
+++ data/items/ItemResource.gd
@@ -2,11 +2,29 @@
 extends TTResource
 class_name ItemResource
 
-enum Slot { NONE, MAIN_HAND, OFF_HAND, ARMOR, HEAD, NECK, RING, BOOTS, BELT }
+enum Category { WEAPON, ARMOR, PACK, TOOL, TRINKET, CONSUMABLE, MISC }
 
-@export var slot: Slot
+enum WeaponType { SIMPLE_MELEE, SIMPLE_RANGED, MARTIAL_MELEE, MARTIAL_RANGED }
+enum ArmorType { LIGHT, MEDIUM, HEAVY, SHIELD }
+
+@export var category: Category = Category.MISC
+
+# Weapon fields
+@export var weapon_type: WeaponType
+@export var damage_die: String = "1d8"
+@export var damage_type: String = "slashing"
+@export var weapon_properties: Array[String] = []
+
+# Armor fields
+@export var armor_type: ArmorType
+@export var armor_class: int = 0
+@export var armor_strength_req: int = 0
+@export var stealth_disadvantage: bool = false
+
+# General fields
 @export var cost_gp: int = 0
 @export var weight_lb: float = 0.0
 @export var modifiers: Array[StatModifier] = []
-@export var is_equipped: bool = false
 @export var requires_attunement: bool = false
+@export var is_container: bool = false
+@export var default_contents: Array[String] = []
