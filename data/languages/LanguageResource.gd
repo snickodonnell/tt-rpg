89544445fa6ea85c4cec66ae01e0900802e5e5e3
snickodonnell <snickodonnell@gmail.com>
2026-03-05 19:07:10 -0600

    Languages and alignments added.

---
+++ data/languages/LanguageResource.gd
@@ -0,0 +1,6 @@
+@tool
+extends TTResource
+class_name LanguageResource
+
+@export var common_name: String = "Common"
+@export var spoken_by: String = "Most humanoids"
---
+++ data/system/AlignmentResource.gd
@@ -0,0 +1,9 @@
+@tool
+extends TTResource
+class_name AlignmentResource
+
+enum Alignment {
+    LAWFUL_GOOD, NEUTRAL_GOOD, CHAOTIC_GOOD,
+    LAWFUL_NEUTRAL, TRUE_NEUTRAL, CHAOTIC_NEUTRAL,
+    LAWFUL_EVIL, NEUTRAL_EVIL, CHAOTIC_EVIL
+}
